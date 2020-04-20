0df8c2bb978178b4c493ff4120041cb4b229e2f2
Guilherme-Martins267 <guihbm.m@gmail.com>
2020-04-19 22:15:22 -0300

    Exemplo de Escopo.

---
+++ src/main/java/Martins/Guilherme/ExemploEscopo/ExemploEscopoApplication.java
@@ -0,0 +1,26 @@
+package Martins.Guilherme.ExemploEscopo;
+
+import Martins.Guilherme.ExemploEscopo.dao.ClassDAO;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class ExemploEscopoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = SpringApplication.run(ExemploEscopoApplication.class, args);
+
+		ClassDAO objectDAO1 = applicationContext.getBean(ClassDAO.class);
+		ClassDAO objectDAO2 = applicationContext.getBean(ClassDAO.class);
+
+		System.out.println("Objeto DAO:" + objectDAO1);
+		System.out.println("Objeto JDBC:" + objectDAO1.getObjectJDBC());
+
+		System.out.println("Objeto DAO:" + objectDAO2);
+		System.out.println("Objeto JDBC:" + objectDAO2.getObjectJDBC());
+
+
+	}
+
+}
